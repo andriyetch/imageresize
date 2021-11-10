692f512a11d9cdf372043751ed1843357462e908
Andriy M Etcheverry <andriy.etcheverry@gmail.com>
2021-11-10 20:17:32 +0400

    Add files via upload

---
+++ imagemin.js
@@ -0,0 +1,70 @@
+import imagemin from 'imagemin';
+import imageminJpegtran from 'imagemin-jpegtran';
+import imageminPngquant from 'imagemin-pngquant';
+import imageminMozJpeg from 'imagemin-mozjpeg';
+
+import Path from 'path';
+import FS from 'fs';
+
+let Files  = [];
+let extensions = ['.png', '.jpg', '.jpeg']
+
+var imageDirectory = "D:\\testtttttttttttt" //paste your img directory here, 
+								//resized images will go into another folder called "resized"
+								//in the same parent directory as the original
+
+const files = async (filepath, newPath) => {
+	try {
+		await imagemin([filepath], {
+			destination: newPath,
+			plugins: [
+				imageminMozJpeg(),
+				imageminPngquant({
+					quality: [0.6, 0.8] //change quality settings here
+				})
+			]
+		});
+	} catch (error) {
+		throw error
+	}
+}
+
+async function ThroughDirectory(Directory) {
+    FS.readdirSync(Directory).forEach(File => {
+        const Absolute = Path.join(Directory, File);
+        if (FS.statSync(Absolute).isDirectory()) return ThroughDirectory(Absolute);
+        else return Files.push(Absolute);
+    });
+}
+
+ThroughDirectory(imageDirectory).then(async () => {
+	var counter = 0
+	var errorFiles = []
+
+	for (let i = 0; i < Files.length; i++) {
+		
+		const adjustedPath = Path.join(imageDirectory, "..", "resized");
+
+		if (extensions.includes(Path.extname(Files[i]).toLowerCase())) {
+			await files(Files[i], adjustedPath).then(() => {
+				console.log()
+				console.log(Files[i])
+				console.log(`End ${i}`)
+			}).catch((error) => {
+				console.log('error')
+				console.log(error)
+				counter++
+				errorFiles.push(Files[i])
+				console.log(`Error on file ${Files[i]}`)
+				console.log(`newPath: ${newPath}`)
+				console.log(`adjustedPath: ${adjustedPath}`)
+			})
+		}
+	}
+	console.log(errorFiles)
+	console.log(`Had an error with the above ${counter} files`)
+});
+
+
+
+//export default files
